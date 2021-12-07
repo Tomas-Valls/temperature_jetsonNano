fcd3cad09fd42aafa94eb7a2eb0cda16ec581416
Tomás Valls <al_valls@hotmail.com>
2021-12-07 17:02:11 -0400

    change docker tag

--- run_docker_hc.sh
+++ run_docker_hc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --name "temperature-docker" \
+docker run --name "temperature_jetsonNano:0.1" \
 -d --privileged --net host --restart "always" \
 python3 -u temperature_test.py 15 #dt seconds
