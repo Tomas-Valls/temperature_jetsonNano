986505a8ad2854bf6859fc1405cf1470d3850d13
Tomás Valls <al_valls@hotmail.com>
2021-12-09 02:47:10 -0400

    change to Jetson

--- run_docker_hc.sh
+++ run_docker_hc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker build -t temperature_jetsonnano:0.1 . \
 docker run  "temperature_jetsonnano:0.1" \
-python3 -u temperature_test.py 15 
+python3 -u temperature_test.py 
--- temperature_test.py
+++ temperature_test.py
@@ -1,7 +1,10 @@
-#import os
+import os
+import subprocess
 def main():
     #temp = os.popen(“cat /sys/devices/virtual/thermal/thermal_zone0/temp”).read()
-    temp='Hello World'
+    temp_cmd = ["cat" ,"/sys/devices/virtual/thermal/thermal_zone0/temp"]
+    temp = subprocess.run(temp_cmd,stdout=subprocess.PIPE)
+    temp = temp.stdout.decode().rstrip()
     print ( temp )
 
 if __name__ == "__main__":
