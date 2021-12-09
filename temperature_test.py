5e10c079fbea65ec24c01b381892b3a98d9a1d28
Tomás Valls <al_valls@hotmail.com>
2021-12-09 04:22:20 -0400

    jetson OS change

--- temperature_test.py
+++ temperature_test.py
@@ -1,10 +1,10 @@
 import os
 import subprocess
 def main():
-    #temp = os.popen(“cat /sys/devices/virtual/thermal/thermal_zone0/temp”).read()
     temp_cmd = ["cat" ,"/sys/devices/virtual/thermal/thermal_zone0/temp"]
     temp = subprocess.run(temp_cmd,stdout=subprocess.PIPE)
     temp = temp.stdout.decode().rstrip()
+    temp=int(temp)/1000
     print ( temp )
 
 if __name__ == "__main__":
